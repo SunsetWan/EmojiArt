dcfe802e8c1163873970b7ab8a645c4fa0db0038
sunset wan <sunsetwan@gmail.com>
2022-05-26 15:34:30 +0800

    feat: finish drag&drop

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -20,3 +20,49 @@ extension CGRect {
         CGPoint(x: midX, y: midY)
     }
 }
+
+extension Array where Element == NSItemProvider {
+    func loadObjects<T>(ofType theType: T.Type,
+                        firstOnly: Bool = false,
+                        using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading
+    {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            provider.loadObject(ofClass: theType) { object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadObjects<T>(ofType theType: T.Type,
+                        firstOnly: Bool = false,
+                        using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading
+    {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            let _ = provider.loadObject(ofClass: theType) { object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+}
+
+extension Character {
+    var isEmoji: Bool {
+        if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
+            return (firstScalar.value >= 0x238d || unicodeScalars.count > 1)
+        } else {
+            return false
+        }
+    }
+}
